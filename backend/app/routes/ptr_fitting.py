517a2c00f54817104a87b3b7bf5310ea1b026db2
bnozdryn <b.nozdryn@gmail.com>
2026-05-25 11:09:15 +0200

    Add improvment to implementations

--- backend/app/routes/ptr_fitting.py
+++ backend/app/routes/ptr_fitting.py
@@ -10,7 +10,6 @@ from app.models.ptr_config import PTRConfig
 router = APIRouter()
 
 class PTRChartData(BaseModel):
-    amplitude_data: List[List[Tuple[float, float]]]
     norm_amplitude_data: List[List[Tuple[float, float]]]
     phase_data: List[List[Tuple[float, float]]]
     results: dict
@@ -22,52 +21,52 @@ def format_for_echarts(x: np.ndarray, y: np.ndarray) -> List[Tuple[float, float]
 
 @router.post("/ptr-fitting", response_model=PTRChartData)
 async def ptr_fitting(
-    l2: float = Form(...),
-    k1: float = Form(...),
     l1: float = Form(...),
+    k1: float = Form(...),
     alfa1: float = Form(...),
-    alfa2: float = Form(...),
+    l2: float = Form(...),
+    rhoc2: float = Form(...),
     alfa3: float = Form(...),
+    k3: float = Form(...),
     r21: float = Form(...),
+    d_pump: float = Form(...),
+    Q: float = Form(...),
+    anisotropy= Form(...),
     weight: float = Form(...),
 
     sample_name: str = Form(...),
-    use_hankel: bool = Form(...),
-
     file: UploadFile = File(...),
+    n_start: int = Form(...),
 ):
     config = PTRConfig(
-        l2=l2,
-        k1=k1,
         l1=l1,
+        k1=k1,
         alfa1=alfa1,
-        alfa2=alfa2,
+        l2=l2,
+        rhoc2=rhoc2,
         alfa3=alfa3,
+        k3=k3,
+        d_pump=d_pump,
+        Q=Q,
+        anisotropy=anisotropy,
         r21=r21,
-        weight=weight,
+        phase_weight=weight,
     )
 
     result = (FittingProcessorBuilder()
               .load_dat_data(file_source=file, sample_name=sample_name)
               .load_config(config)
+              .set_starting_point_count(n_start)
               .build()
               .process())
 
-    # Reszta kodu bez zmian...
-    freq = result.frequency_vector
+    freq = result.frequency_hz
     freq_log = np.log10(freq)
 
-    amplitude_data = [
-        format_for_echarts(freq, result.model_amp),
-        format_for_echarts(freq, result.exp_amp),
-    ]
-
-    norm_amp_model = result.model_amp / result.model_amp[0]
-    norm_amp_exp = result.exp_amp / result.exp_amp[0]
 
     norm_amplitude_data = [
-        format_for_echarts(freq_log, np.log10(norm_amp_model)),
-        format_for_echarts(freq_log, np.log10(norm_amp_exp)),
+        format_for_echarts(freq_log, result.model_amp),
+        format_for_echarts(freq_log, result.model_amp),
     ]
 
     phase_data = [
@@ -76,14 +75,16 @@ async def ptr_fitting(
     ]
 
     return PTRChartData(
-        amplitude_data=amplitude_data,
         norm_amplitude_data=norm_amplitude_data,
         phase_data=phase_data,
         results={
+            "anisotropy": result.anisotropy,
             "k2": float(result.k2),
-            "alfa2r": float(result.alfa2),
+            "alfa2": float(result.alfa2),
             "r32": float(result.r32),
-            "phi0_deg": float(result.phi0_deg),
+            "kParallel": float(result.k_parallel),
+            "r2Amp": float(result.r2_amp),
+            "r2Phase": float(result.r2_phase),
             "res_norm": float(result.res_norm),
         }
     )
